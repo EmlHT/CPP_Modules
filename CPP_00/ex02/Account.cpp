a7339ebb02c56845d3aac24e32140eeeaa932e4c
EmlHT <emilien.houot@gmail.com>
2024-02-15 00:44:30 +0100

    ex02 near the end

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: ehouot <ehouot@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:49:18 by ehouot            #+#    #+#             */
-/*   Updated: 2024/02/05 19:13:52 by ehouot           ###   ########.fr       */
+/*   Updated: 2024/02/13 20:53:05 by ehouot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,62 +14,109 @@
 #include <iostream>
 #include <ctime>
 
-int	Account::getNbAccounts( void ) {
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
+int	Account::getNbAccounts( void )
+{
     return (_nbAccounts);
 }
 
-int	Account::getTotalAmount( void ) {
-
+int	Account::getTotalAmount( void )
+{
     return (_totalAmount);
 }
 
-int	Account::getNbDeposits( void ) {
-
+int	Account::getNbDeposits( void )
+{
     return (_totalNbDeposits);
 }
 
-int	Account::getNbWithdrawals( void ) {
-
+int	Account::getNbWithdrawals( void )
+{
     return (_totalNbWithdrawals);
 }
 
-void    Account::_displayTimestamp( void ) {
+void    Account::_displayTimestamp( void )
+{
+    char buffer[80];
 
-    std::cout << "[" << localtime(0) << "]" << std::endl;
+	std::time_t now = std::time(0);
+    std::strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", std::localtime(&now));
+    std::cout << "[" << buffer << "]";
 }
 
-void	Account::displayAccountsInfos( void ) {
-
+void	Account::displayAccountsInfos( void )
+{
     _displayTimestamp();
     std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
-void	makeDeposit( int deposit );
-bool	makeWithdrawal( int withdrawal );
-// int		checkAmount( void ) const;
-// void	displayStatus( void ) const;
+void	Account::makeDeposit( int deposit )
+{	
+	int	p_amount = checkAmount();
 
+	_amount =+ deposit;
+	_totalAmount =+ deposit;
+	_nbDeposits =+ 1;
+	_totalNbDeposits =+ 1;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";deposit:" << deposit << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{	
+	int	p_amount = checkAmount();
+
+	if (p_amount < withdrawal)
+	{
+		_displayTimestamp();
+		std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";withdrawal:refused" << std::endl;
+		return (false);
+	}
+	else
+	{
+		_amount =- withdrawal;
+		_totalAmount =- withdrawal;
+		_nbWithdrawals =+ 1;
+		_totalNbWithdrawals =+ 1;
+	}
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount << ";withdrawal:" << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+	return (true);
+}
+
+int	Account::checkAmount( void ) const
+{	
+	return (_amount);
+}
+
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
 
 Account::Account (void)
 {
-    std::cout << "Constructeur called" << std::endl;
-    getNbAccounts();
-    return ;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << 0 << ";created" << std::endl;
+	return ;
 }
 
 Account::Account( int initial_deposit )
 {
-	std::cout << "index:0;amount:42;created" << std::endl;
-    Account::_nbDeposits = 1;
-    _amount = initial_deposit;
-    return ;
+	_amount = initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+	return ;
 }
 
 Account::~Account (void)
 {
-    std::cout << "Destructeur called" << std::endl;
-    return ;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;;
+	return ;
 }
-
-

8777cdcb5a367ac1b730716d7286108b7fc4ba42
EmlHT <emilien.houot@gmail.com>
2024-02-20 18:56:53 +0100

    CPP01 ex04 loading

---
+++ CPP_01/ex04/replace.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehouot <ehouot@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 18:15:00 by ehouot            #+#    #+#             */
+/*   Updated: 2024/02/20 18:24:00 by ehouot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+
+# include <iostream>
+# include <string>
+
+class Replace {
+
+public :
+
+    Replace( std::string filename, std::string s1, std::string s2);
+    ~Replace();
+
+    replace ()
+    
+};
+
+#endif

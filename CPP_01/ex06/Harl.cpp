5facef90092288713a9ec023292affeae1a84ac1
EmlHT <emilien.houot@gmail.com>
2024-02-26 16:35:57 +0100

    Module CPP_01 Done

--- CPP_01/ex06/Harl.cpp
+++ CPP_01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ehouot <ehouot@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 15:13:14 by ehouot            #+#    #+#             */
-/*   Updated: 2024/02/26 15:03:14 by ehouot           ###   ########.fr       */
+/*   Updated: 2024/02/26 15:51:32 by ehouot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,63 @@
 
 void	Harl::debug( void )
 {
-    std::cout << "DEBUG :" << std::endl;
+    std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << std::endl;
 }
 
 void	Harl::info( void )
 {
-	std::cout << "INFO :" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << std::endl;
 }
 
 void	Harl::warning( void )
 {
-	std::cout << "WARNING :" << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << std::endl;
 }
 
 void	Harl::error( void )
 {
-	std::cout << "ERROR :" << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
 }
 
 void	Harl::complain( std::string level )
 {
-	void (Harl::*actions[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int i = 0;
 	std::string	listLevel[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	for (int i = 0; i != 4; i++)
+	for (i = 0; i != 4; i++)
 	{
 		if (listLevel[i].compare(level) == 0)
-		{
-			(this->*actions[i])();
-			return ;
-		}
+			break ;
 	}
-	std::cout << "Not available entry" << std::endl;
+    switch (i)
+	{
+		case 0:
+			debug();
+		case 1:
+			info();
+		case 2:
+			warning();
+		case 3:
+			error();
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 }
 
 Harl::Harl( void )
 {
-	std::cout << "Constructor called" << std::endl;
 	return ;
 }
 
 Harl::~Harl( void )
 {
-	std::cout << "Destructor called" << std::endl;
 	return ;
 }
--- CPP_01/ex06/main.cpp
+++ CPP_01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ehouot <ehouot@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 15:30:06 by ehouot            #+#    #+#             */
-/*   Updated: 2024/02/26 14:57:51 by ehouot           ###   ########.fr       */
+/*   Updated: 2024/02/26 15:25:33 by ehouot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,16 @@
 #include <iomanip>
 #include <limits>
 
-bool	cin_error(void) {
-
-	if (std::cin.eof())
-	{
-		std::cout << "\033[1;35mEOF detected. Exiting...\033[0m" << std::endl;
-		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		return (false);
-	}
-	if (std::cin.fail())
-	{
-		std::cout << "\033[1;35mError in input. Exiting...\033[0m" << std::endl;
-		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		return (false);
-	}
-	return (true);
-}
-
-int main()
+int main(int argc, char **argv)
 {
-	std::string level;
-	Harl	Harl;
+	Harl		Harl;
 
-	std::cout << "Enter DEBUG, INFO, WARNING or ERROR" << std::endl;
-	std::cout << "(Ctrl + D : Quit program)" << std::endl;
-	while (1)
+	if (argc != 2)
 	{
-		std::getline(std::cin, level);
-		if (!cin_error())
-			return (1);
-		Harl.complain(level);	
+		std::cout << "Wrong number of arguments" << std::endl;
+		return (1);
 	}
+	std::string	level = argv[1];
+	Harl.complain(level);	
 	return (0);
 }

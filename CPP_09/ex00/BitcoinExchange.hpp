3f9e2910842027b88b81b356bac9c3842689ddb1
EmlHT <emilien.houot@gmail.com>
2024-05-02 20:10:14 +0200

    Begining of CPP_09

---
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehouot <ehouot@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 18:40:12 by ehouot            #+#    #+#             */
+/*   Updated: 2024/05/02 18:41:11 by ehouot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+class BitcoinExchange
+{
+    public :
+
+    
+    
+    private :
+
+    
+};
